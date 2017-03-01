3644bbdcf4e81a62bc154de6a8b54c86b2d3ea34
sahawut <sahawut@gmail.com>
2017-03-01 13:33:53 -0500

    Refactored app.js.

--- app.js
+++ app.js
@@ -1,61 +1,31 @@
 window.addEventListener('load', function() {
   var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
-  var USER_ROUTE = "/user.html";
-  var ADMIN_ROUTE = "/admin.html";
 
-  var btn_login = document.getElementById('btn-login');
-  if (btn_login) {
-    btn_login.addEventListener('click', function() {
-      lock.show();
-    });
-  }
+  addClickListener(document.getElementById('btn-login'), lock.show);
+  addClickListener(document.getElementById('btn-logout'), logout);
+  lock.on("authenticated", onAuthenticated);
+  route();
 
-  document.getElementById('btn-logout').addEventListener('click', function() {
-    logout();
-  });
-
-  lock.on("authenticated", function(authResult) {
+  function onAuthenticated(authResult) {
     lock.getProfile(authResult.idToken, function (err, profile) {
       if (err) {
-        // Remove expired token (if any)
-        localStorage.removeItem('id_token');
-        // Remove expired profile (if any)
-        localStorage.removeItem('profile');
+        removeExpiredIfAny('id_token');
+        removeExpiredIfAny('profile');
         return alert('There was an error getting the profile: ' + err.message);
-      } else {
-        localStorage.setItem('id_token', authResult.idToken);
-        localStorage.setItem('profile', JSON.stringify(profile));
-        route();
       }
+      localStorage.setItem('id_token', authResult.idToken);
+      localStorage.setItem('profile', JSON.stringify(profile));
+      route();
     });
-  });
+    var removeExpiredIfAny = localStorage.removeItem;
+  }
 
-  var isAdmin = function(profile) {
-    if (profile &&
-        profile.app_metadata &&
-        profile.app_metadata.roles &&
-        profile.app_metadata.roles.indexOf('ISG_ADMIN') > -1) {
-      return true;
-    } else {
-       return false;
-    }
-  };
+  function route() {
+    var USER_ROUTE = "/user.html";
+    var ADMIN_ROUTE = "/admin.html";
 
-  var isUser = function(profile) {
-    if (profile &&
-        profile.app_metadata &&
-        profile.app_metadata.roles &&
-        profile.app_metadata.roles.indexOf('ISG_USER') > -1) {
-      return true;
-    } else {
-       return false;
-    }
-  };
-
-  var route = function() {
-    var id_token = localStorage.getItem('id_token');
     var locationPath = window.location.pathname;
-    if (id_token) {
+    if (localStorage.getItem('id_token')) {
       var profile = JSON.parse(localStorage.getItem('profile'));
       var route = locationPath.replace(CONTEXT, '');
       switch(route) {
@@ -63,12 +33,12 @@ window.addEventListener('load', function() {
           hide(document.getElementById('btn-login'));
           show(document.getElementById('btn-logout'));
           var baseUrl = window.location.href.split(CONTEXT)[0] + CONTEXT;
-          if (isAdmin(profile)){
+          if (hasRole(profile, 'ISG_ADMIN')){
             var adminBtn = document.getElementById('btn-go-admin');
             adminBtn.href = baseUrl + ADMIN_ROUTE;
             show(adminBtn);
           }
-          if (isUser(profile)){
+          if (hasRole(profile, 'ISG_USER')){
               var uerBtn = document.getElementById('btn-go-user');
               uerBtn.href = baseUrl + USER_ROUTE;
               show(document.getElementById('btn-go-user'));
@@ -92,28 +62,39 @@ window.addEventListener('load', function() {
             document.getElementById('nickname').textContent = profile.nickname;
           }
           break;
-      };
+      }
     } else { // user is not logged in.
       // Call logout just to be sure our local session is cleaned up.
       if (CONTEXT + '/' != locationPath) {
         logout();
       }
     }
-  };
+  }
 
-  var logout = function() {
+  function logout() {
     localStorage.removeItem('id_token');
     localStorage.removeItem('profile');
     window.location.href = CONTEXT;
-  };
+  }
 
-  var hide = function(element) {
+  function hide(element) {
     element.style.display = "none";
-  };
+  }
 
-  var show = function(element) {
+  function show(element) {
     element.style.display = "inline-block";
-  };
+  }
 
-  route();
+  function addClickListener(button, callback) {
+    if (button) {
+      button.addEventListener('click', callback);
+    }
+  }
+
+  function hasRole(profile, role) {
+    return !!(profile &&
+    profile.app_metadata &&
+    profile.app_metadata.roles &&
+    profile.app_metadata.roles.indexOf(role) > -1);
+  }
 });
