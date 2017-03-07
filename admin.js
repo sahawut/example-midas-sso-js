ffca243fe57bc888d0bf0a507db64717baaca56e
sahawut <sahawut@gmail.com>
2017-03-07 14:26:09 -0500

    Rolled back to version 0.0.2.

--- admin.js
+++
@@ -1,9 +0,0 @@
-/*
-admin page-specific script
-*/
-
-(function displayAdmin(HELPER) {
-	document.getElementById('content').innerHTML = "This is the Administrator Page.";
-	
-	return;
-})(window.parent.HELPER);
--- auth0-variables.js
+++ auth0-variables.js
@@ -1,10 +1,4 @@
-/*
-Please update the permissions at https://manage.auth0.com/ as well as these variables after redeploying this project.
-*/
-
-var AUTH0_VARIABLES = {
-	clientID: '9SdYbDLnKMXKQi6Dt13Mi3eu07iQitFx',
-	domain: 'midas-dev-sso.auth0.com',
-	callbackURL: location.href,
-	midasAccountsURL: "https://betaweb.rods.pitt.edu/hub-alpha/sso"
-};
+var AUTH0_CLIENT_ID='9SdYbDLnKMXKQi6Dt13Mi3eu07iQitFx';
+var AUTH0_DOMAIN='midas-dev-sso.auth0.com';
+var AUTH0_CALLBACK_URL=location.href;
+var CONTEXT = '/example-midas-sso-js';
