7dcb6eb85fd3984a3ca6e3785202f4dd5fee5a85
sahawut <sahawut@gmail.com>
2017-03-07 15:54:47 -0500

    Added MIDAS SSO.

--- app.js
+++ app.js
@@ -1,101 +1,190 @@
 window.addEventListener('load', function() {
-  var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
+    var auth0 = newAuth0();
 
-  addClickListener(document.getElementById('btn-login'), function(){lock.show();});
-  addClickListener(document.getElementById('btn-logout'), logout);
-  lock.on("authenticated", onAuthenticated);
-  route();
-
-  function onAuthenticated(authResult) {
-    lock.getProfile(authResult.idToken, function (err, profile) {
-      if (err) {
-        removeExpiredIfAny('id_token');
-        removeExpiredIfAny('profile');
-        return alert('There was an error getting the profile: ' + err.message);
-      }
-      localStorage.setItem('id_token', authResult.idToken);
-      localStorage.setItem('profile', JSON.stringify(profile));
-      route();
-    });
-    var removeExpiredIfAny = localStorage.removeItem;
-  }
-
-  function route() {
-    var USER_ROUTE = "/user.html";
-    var ADMIN_ROUTE = "/admin.html";
-
-    var locationPath = window.location.pathname;
-    if (localStorage.getItem('id_token')) {
-      var profile = JSON.parse(localStorage.getItem('profile'));
-      var route = locationPath.replace(CONTEXT, '');
-      switch(route) {
-		case "":
-        case "/":
-          hide(document.getElementById('btn-login'));
-          show(document.getElementById('btn-logout'));
-          var baseUrl = window.location.href.split(CONTEXT)[0] + CONTEXT;
-          if (hasRole(profile, 'ISG_ADMIN')){
-            var adminBtn = document.getElementById('btn-go-admin');
-            adminBtn.href = baseUrl + ADMIN_ROUTE;
-            show(adminBtn);
-          }
-          if (hasRole(profile, 'ISG_USER')){
-              var uerBtn = document.getElementById('btn-go-user');
-              uerBtn.href = baseUrl + USER_ROUTE;
-              show(document.getElementById('btn-go-user'));
-          }
-          break;
-        case USER_ROUTE:
-          if (true != hasRole(profile, 'ISG_USER')) {
-            window.location.href = CONTEXT;
-          } else {
-            show(document.querySelector('.container'));
-            show(document.getElementById('btn-logout'));
-            document.getElementById('nickname').textContent = profile.nickname;
-          }
-          break;
-        case ADMIN_ROUTE:
-          if (true != hasRole(profile, 'ISG_ADMIN')) {
-            window.location.href = CONTEXT;
-          } else {
-            show(document.querySelector('.container'));
-            show(document.getElementById('btn-logout'));
-            document.getElementById('nickname').textContent = profile.nickname;
-          }
-          break;
-      }
-    } else { // user is not logged in.
-      // Call logout just to be sure our local session is cleaned up.
-      if (CONTEXT + '/' != locationPath) {
-        logout();
-      }
+    if (doesUserLogInLocally()) {
+        showThePage();
+    } else {
+        var isHandlingCallbackFromAuth0 = handleCallbackFromAuth0(toAuthResultFromLocationHash());
+        if (! isHandlingCallbackFromAuth0)
+            auth0.getSSOData(onSsoDataHandlingAuthentication);
     }
-  }
 
-  function logout() {
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('profile');
-    window.location.href = CONTEXT;
-  }
+    setInterval(logoutIfLoggedOutGloballyByAnotherApp, 5000);
 
-  function hide(element) {
-    element.style.display = "none";
-  }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Helper function section
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    function handleCallbackFromAuth0(authResult) {
+        if (authResult.idToken){
+            new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN).getProfile(authResult.idToken, onUserProfile);
+            return true;
+        }
+        return false;
 
-  function show(element) {
-    element.style.display = "inline-block";
-  }
-
-  function addClickListener(button, callback) {
-    if (button) {
-      button.addEventListener('click', callback);
+        function onUserProfile(error, profile) {
+            if (! error) {
+                localStorage.setItem(KEY_LOCAL_STORAGE_USER_TOKEN, authResult.idToken);
+                localStorage.setItem(KEY_LOCAL_STORAGE_PROFILE, JSON.stringify(profile));
+                showThePage(authResult.state);
+            }
+        }
     }
-  }
 
-  function hasRole(profile, role) {
-    return !!(profile &&
-    profile.app_metadata &&
-    profile.app_metadata.roles &&
-    profile.app_metadata.roles.indexOf(role) > -1);
-  }
+    function onSsoDataHandlingAuthentication(err, data) {
+        var hasSsoSession = !err && data && data.sso;
+        if (hasSsoSession)
+            redirectToAuth0ForSso();
+        else
+            redirectTo(toMidasAccountsUrl('/sso'));
+
+        function redirectToAuth0ForSso() {
+            auth0.signin({
+                connection: data.lastUsedConnection.name,
+                state: window.location.href,
+                scope: 'openid name picture roles'
+            });
+        }
+    }
+
+    function logout() {
+        localStorage.removeItem(KEY_LOCAL_STORAGE_USER_TOKEN);
+        localStorage.removeItem(KEY_LOCAL_STORAGE_PROFILE);
+        redirectTo(toMidasAccountsUrl('/signoff', 'Logged off successfully'));
+    }
+
+    function logoutIfLoggedOutGloballyByAnotherApp() {
+        if (! doesUserLogInLocally()) return;
+        auth0.getSSOData(onSsoDataHandlingLogoutIfNoSsoSession);
+
+        function onSsoDataHandlingLogoutIfNoSsoSession(err, data) {
+            var hasSsoSession = !err && data && data.sso;
+            if (! hasSsoSession)
+                logout();
+        }
+    }
+
+    function newAuth0() {
+        return new Auth0({
+            domain: AUTH0_DOMAIN,
+            clientID: AUTH0_CLIENT_ID,
+            callbackURL: 'http://localhost:3000' + CONTEXT + '/',
+            callbackOnLocationHash: true
+        });
+    }
+
+    function doesUserLogInLocally() {
+        return !! (localStorage.getItem(KEY_LOCAL_STORAGE_USER_TOKEN));
+    }
+
+    function showThePage(targetUrl) {
+        if (targetUrl)
+            redirectTo(targetUrl);
+
+        var locationPath = window.location.pathname;
+        document.body.style.display = 'inline';
+        routeByUserRole(locationPath.replace(CONTEXT, ''));
+
+        function routeByUserRole(route) {
+            var profile = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE_PROFILE));
+            switch (route) {
+                case "":
+                case "/":
+                    showLandingPageComponents();
+                    break;
+                case ROUTE_USER:
+                    secureRoute(ROLE_ISG_USER);
+                    break;
+                case ROUTE_ADMIN:
+                    secureRoute(ROLE_ISG_ADMIN);
+                    break;
+            }
+
+            function showLandingPageComponents() {
+                var baseUrl = window.location.href.split(CONTEXT)[0] + CONTEXT;
+                hide(document.getElementById('btn-login'));
+                showCommonComponentsInAllPages();
+                if (hasRole(profile, ROLE_ISG_ADMIN)) {
+                    rewireAndShowButtonById('btn-go-admin', baseUrl + ROUTE_ADMIN);
+                }
+                if (hasRole(profile, ROLE_ISG_USER)) {
+                    rewireAndShowButtonById('btn-go-user', baseUrl + ROUTE_USER);
+                }
+            }
+
+            function secureRoute(requiredRole) {
+                if (!hasRole(profile, requiredRole)) {
+                    if (profile)
+                        redirectTo('unauthorized.html');
+                    else
+                        redirectTo(CONTEXT);
+
+                } else{
+                    showCommonComponentsInAllPages();
+                }
+            }
+
+            function showCommonComponentsInAllPages() {
+                var buttonLogout = document.getElementById('btn-logout');
+                show(buttonLogout);
+                addClickListener(buttonLogout, logout);
+                show(document.querySelector('.container'));
+                document.getElementById('nickname').textContent = profile.nickname;
+            }
+
+            function rewireAndShowButtonById(id, route) {
+                var button = document.getElementById(id);
+                button.href = route;
+                show(button);
+            }
+        }
+    }
+
+    function toAuthResultFromLocationHash() {
+        return {
+            idToken: getParameterByName('id_token'),
+            state: getParameterByName('state')
+        };
+    }
+
+    function toMidasAccountsUrl(endpoint, message, title) {
+        title = title || "JS Example";
+        message = message || "Please login to use the services";
+        return MIDAS_ACCOUNTS_URL + endpoint + '?returnToUrl='
+            + encodeURIComponent(window.location) + '&title=' + title + '&message=' + message;
+    }
+
+    function hasRole(profile, role) {
+        return !!(profile &&
+        profile.app_metadata &&
+        profile.app_metadata.roles &&
+        profile.app_metadata.roles.indexOf(role) > -1);
+    }
+
+    function hide(element) {
+        element.style.display = "none";
+    }
+
+    function show(element) {
+        element.style.display = "inline-block";
+    }
+
+    function addClickListener(button, callback) {
+        if (button) {
+            button.addEventListener('click', callback);
+        }
+    }
+
+    function redirectTo(href) {
+        window.location.href = href;
+    }
+
+    function getParameterByName(name, url) {
+        name = name.replace(/[\[\]]/g, "\\$&");
+        url = url || window.location.href;
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 });
--- auth0-variables.js
+++ auth0-variables.js
@@ -1,4 +1,16 @@
-var AUTH0_CLIENT_ID='9SdYbDLnKMXKQi6Dt13Mi3eu07iQitFx';
+// Auth0
 var AUTH0_DOMAIN='midas-dev-sso.auth0.com';
-var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_CLIENT_ID='9SdYbDLnKMXKQi6Dt13Mi3eu07iQitFx';
+var MIDAS_ACCOUNTS_URL = 'https://betaweb.rods.pitt.edu/hub-alpha';
+
+// Single Page Application
 var CONTEXT = '/example-midas-sso-js';
+var KEY_LOCAL_STORAGE_USER_TOKEN = 'userToken';
+var KEY_LOCAL_STORAGE_PROFILE = 'profile';
+
+var ROUTE_USER = "/user.html";
+var ROUTE_ADMIN = "/admin.html";
+
+// MIDAS Accounts
+var ROLE_ISG_USER = 'ISG_USER';
+var ROLE_ISG_ADMIN = 'ISG_ADMIN';
